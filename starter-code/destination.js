aac72edfbb78ce6a29c4582781ba240529501d48
milicaca <sukovic.milica@example.com>
2023-04-03 11:48:17 +0200

    planets + js

---
+++ starter-code/destination.js
@@ -0,0 +1,6 @@
+function changeImg() {
+    document.getElementById("mars").src = "assets/destination/image-mars.webp";
+}
+function changeMsg(){
+    document.getElementsById("message").innerHTML = "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!"
+}
